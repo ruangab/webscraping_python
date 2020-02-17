a768bd8167099784a358ae23706542c20a30d7f3
ruangab <ruangab2@gmail.com>
2020-02-17 15:47:59 -0300

    Primeiro commit

--- trending.py
+++ trending.py
@@ -1,10 +1,16 @@
 import requests as req
 from bs4 import BeautifulSoup
 
+# url do site
 url = "https://www.youtube.com/feed/trending"
+
+# baixando o conteúdo do site
 page = req.get(url)
+
+# pegando o conteúdo do site e transferindo para o analizador
 soup = BeautifulSoup(page.text, 'html.parser')
 
+#pegando todos os conteúdos das div's com a seguinte classe
 for titles in soup.find_all(class_='yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 yt-uix-sessionlink spf-link'):
     print("")
     print(titles.text)
